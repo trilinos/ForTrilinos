4669e3c067575cc63d9bb445a2fdd4255f8439f8
Damian Rouson <rouson@sandia.gov>
2009-05-19 14:40:08 +0000

    SUMMARY:   [removing outdated  procedural test simpleEpetraFortran.f03 and redundant simpleEpetraExteranCInterface.c]
         BUGS:      [???]
    
         CHECKLIST:
    
         1)   Did you add any source files?  [no]
    
         2)   Did you update or change the build system?  [no]
    
         3) Did you rerun the test suite on at least the packages that you
            changed?  [Not yet -- need to finish CMakeifying first.]
    
         COMMENTS:
    
         [???]
    
    (checklist version 2007-06-20)

--- test/dummy.cpp
+++ test/dummy.cpp
@@ -1,2 +1,5 @@
 // This C++ file is just here to trick autotools into using the C++ linker
-// when linking the *.c program!  Am I smart or what!
+// when linking the *.f03 programs.  Using the C++ Linker is less involved than
+// using the Fortran linker because the number libraries that must be explicitly
+// linked is lower.  This idea was copied from CTrilinos.
+// 
