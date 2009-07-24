48ff6119d7174c62f0c13e98cc46a886f0305a8f
Damian Rouson <rouson@sandia.gov>
2009-07-24 21:26:33 +0000

    SUMMARY:   [Removed dummy.cpp previously needed to convince autotools to link code including Fortran with the C++ compiler.]
         BUGS:      [no]
    
         CHECKLIST:
    
         1)   Did you add any source files?  [no]
    
         2)   Did you update or change the build system?  [no]
    
         3) Did you rerun the test suite on at least the packages that you
            changed?  [no]
    
         COMMENTS:
    
         [???]
    
    (checklist version 2007-06-20)

--- test/dummy.cpp
+++
@@ -1,5 +0,0 @@
-// This C++ file is just here to trick autotools into using the C++ linker
-// when linking the *.f03 programs.  Using the C++ Linker is less involved than
-// using the Fortran linker because the number libraries that must be explicitly
-// linked is lower.  This idea was copied from CTrilinos.
-// 
