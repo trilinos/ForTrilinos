f47ed2f0e48a8063fab494de3db0f25e61a0591d
M. Nicole Lemaster <mnlemas@sandia.gov>
2009-09-15 18:43:59 +0000

    SUMMARY:  Modified ForTrilinos interoperability check.
         BUGZILLA BUG #(s):
    
         CHECKLIST
         ---------
    
         1) Did you 'cvs add' all new files and directories [Y:N:N/A]?  N/A
    
         2) Did you add new source files to a CMakeLists.txt file [Y:N:N/A]?  N/A
    
         3)  Did you modify or add any source or build system files?  Y
         3a) Did you rebuild the libraries and executables?  Y
         3b) Did you rerun the test suite on the packages that you changed and
             all packages that depend on the modified packages?  N
         3c) Did you change the required or optional dependencies for any
             package?  N
    
    
         DESCRIPTION OF CHANGES:
    
         Made ForTrilinos interoperability check compatible with ctest.
    
    (checklist version 2009-07-14)

--- test/interoperability_check.F90
+++ test/interoperability_check.F90
@@ -3,8 +3,10 @@ program main
   use iso_fortran_env   ,only : error_unit ,output_unit
   implicit none
   if (valid_kind_parameters(verbose=.true.)) then
-    write(output_unit,fmt='(a)') 'interoperability_check.F90: test passed. :)'
+    write(output_unit,*) 
+    write(output_unit,fmt='(a)') "End Result: TEST PASSED" ;
   else
-    write(error_unit,fmt='(a)') 'interoperability_check.F90: test failed. :('
+    write(error_unit,*) 
+    write(error_unit,fmt='(a)') "End Result: TEST FAILED" ;
   end if
 end program
