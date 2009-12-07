56765db3c8bdc311cda579555b9d533892d35507
Damian Rouson <rouson@sandia.gov>
2009-12-07 15:27:26 -0800

    Adding 'use' dependancy of fortechos.F90 on ForTrilinos_enum_wrappers.F90

--- src/teuchos/forteuchos.F90
+++ src/teuchos/forteuchos.F90
@@ -1,6 +1,7 @@
 module forteuchos
   use iso_c_binding ,only : c_int,c_double,c_char,c_bool,c_ptr,c_long,c_float
   use ForTrilinos_enums
+  use ForTrilinos_enum_wrappers
   implicit none   ! Prevent implicit typing
 
   ! This file provides Fortran interface blocks that bind the argument types,
