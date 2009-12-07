d4da93ae024de40c0d514a123fd7bb846571d4d3
Damian Rouson <rouson@sandia.gov>
2009-12-07 15:03:08 -0800

    Making epetra_serialcomm a child of epetra_comm.

--- src/epetra/FEpetra_SerialComm.F90
+++ src/epetra/FEpetra_SerialComm.F90
@@ -1,48 +1,30 @@
 module FEpetra_SerialComm
   use ForTrilinos_enums ,only : FT_Epetra_SerialComm_ID_t,ForTrilinos_Object_ID_t
-  use ForTrilinos_universal ,only : universal
+  use FEPetra_Comm ,only : Epetra_Comm
   use forepetra
   private                     ! Hide everything by default
-  public :: epetra_serialcomm ! Expose type/constructor/methods
+  public :: epetra_serialcomm ! Expose type/constructors/methods
   implicit none
 
-  type ,extends(universal) :: epetra_serialcomm
+  type ,extends(Epetra_Comm) :: epetra_serialcomm
     private
     type(FT_Epetra_SerialComm_ID_t) :: selfID
   contains
-    procedure :: clone
-    procedure :: generalize => epetra_serialcomm_generalize
-    procedure :: force_finalization => call_finalize
-    final :: finalize
+     procedure :: generalize 
+     procedure :: force_finalization 
+     procedure :: clone 
+     final :: finalize
   end type
 
-  interface epetra_serialcomm ! constructors
-    procedure from_scratch,duplicate
-  end interface
-
+   interface epetra_serialcomm ! constructors
+     module procedure from_scratch,duplicate,from_struct
+   end interface
+ 
 contains
 
-  ! CTrilinos prototype:
-  ! CT_Epetra_SerialComm_ID_t Epetra_SerialComm_Cast ( CTrilinos_Object_ID_t id );
-
-
-  ! CTrilinos prototype:
-  ! CTrilinos_Object_ID_t Epetra_SerialComm_Abstract ( CT_Epetra_SerialComm_ID_t id );
-
-  type(ForTrilinos_Object_ID_t) function epetra_serialcomm_generalize(this)
-    use iso_c_binding ,only : c_loc
-    class(epetra_serialcomm) ,intent(in) ,target :: this
-
-   !interface
-   !  type(ForTrilinos_Object_ID_t) function FEpetra_SerialComm_Abstract(object_id_ptr) bind(C,name="for_linking_only")
-   !    use iso_c_binding ,only : c_ptr
-   !    import :: ForTrilinos_Object_ID_t
-   !    type(c_ptr) ,value :: object_id_ptr
-   !  end function
-   !end interface
-
-    epetra_serialcomm_generalize = Epetra_SerialComm_Abstract ( this%selfID )
-   !epetra_serialcomm_generalize = FEpetra_SerialComm_Abstract( c_loc(this%selfID) )
+  type(epetra_serialcomm) function from_struct(id)
+     type(FT_Epetra_SerialComm_ID_t) ,intent(in) :: id
+     from_struct%selfID = id
   end function
 
   ! Original C++ prototype:
@@ -64,30 +46,46 @@ contains
     duplicate%selfID = Epetra_SerialComm_Duplicate(original%selfID)
   end function
 
-  ! Original C++ prototype:
-  ! Epetra_Comm * Clone() const;
-  ! CTrilinos prototype:
-  ! CT_Epetra_Comm_ID_t Epetra_SerialComm_Clone ( CT_Epetra_SerialComm_ID_t selfID );
- 
-  type(epetra_comm) function clone(this)
-    use FEpetra_Comm ,only : epetra_comm
+  function clone(this)
     class(epetra_serialcomm) ,intent(in) :: this
-    clone = Epetra_SerialComm_Clone(this%selfID)
+    class(epetra_comm) ,allocatable :: clone
+    allocate(epetra_serialcomm :: clone) 
+    select type(clone)
+      class is (epetra_serialcomm)
+       !clone%selfID = Epetra_SerialComm_Clone(this%selfID)
+        stop 'epetra_serialcomm%clone(): determine return type now that epetra_comm is abstract'
+      class default
+        stop 'epetra_serialcomm%clone(): class not supported'
+    end select
   end function
-
-  ! Original C++ prototype:
-  ! virtual ~Epetra_SerialComm();
-  ! CTrilinos prototype:
-  ! void Epetra_SerialComm_Destroy ( CT_Epetra_SerialComm_ID_t * selfID );
 
   subroutine finalize(this)
     type(epetra_serialcomm) :: this
-    call Epetra_SerialComm_Destroy ( this%selfID ) 
+    call Epetra_SerialComm_Destroy( this%selfID ) 
   end subroutine
 
-  subroutine call_finalize(this)
+  subroutine force_finalization(this)
     class(epetra_serialcomm) ,intent(inout) :: this
     call finalize(this) 
   end subroutine
 
+  type(ForTrilinos_Object_ID_t) function generalize(this)
+    class(epetra_serialcomm) ,intent(in) ,target :: this
+
+    generalize = Epetra_SerialComm_Abstract ( this%selfID )
+
+   !Alternate implementation:
+   !use iso_c_binding ,only : c_loc
+   !interface
+   !  type(ForTrilinos_Object_ID_t) function FEpetra_SerialComm_Abstract(object_id_ptr) bind(C,name="for_linking_only")
+   !    use iso_c_binding ,only : c_ptr
+   !    import :: ForTrilinos_Object_ID_t
+   !    type(c_ptr) ,value :: object_id_ptr
+   !  end function
+   !end interface
+   !epetra_serialcomm_generalize = FEpetra_SerialComm_Abstract( c_loc(this%selfID) )
+
+  end function
+ 
 end module 
+
