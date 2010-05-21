89bf953de3d74e1de237aa3b5acf458de1322693
Nicole Lemaster <mnlemas@sandia.gov>
2010-05-21 13:28:07 -0700

    End module must appear on its own line or xlf gets cranky.
    =============================
    Build/Test Cases Summary
    =============================
    
    Fri May 21 13:30:42 PDT 2010
    
    Enabled Packages: ForTrilinos
    Enabled all Forward Packages
    
    Build test results:
    -------------------
    0) MPI_DEBUG => passed: Trilinos/MPI_DEBUG: passed=2,notpassed=0 (0.93593651851 min)
    1) SERIAL_RELEASE => passed: Trilinos/SERIAL_RELEASE: passed=3,notpassed=0 (0.761566066742 min)
    
    Local commits for this build/test group:
    ----------------------------------------
    cdd281c End module must appear on its own line or xlf gets cranky.

--- test/unittests/fortran_driver_macros.h
+++ test/unittests/fortran_driver_macros.h
@@ -10,24 +10,30 @@
 
 #  define FORTRILINOS_UNITTEST_MODULE_DEF0(CLASSNAME) \
     module TEST_CALLS_FILE(CLASSNAME) BREAKLINE \
-      use TEST_FILE(CLASSNAME) BREAKLINE \
-      implicit none BREAKLINE \
-      public BREAKLINE \
-      contains
+      use TEST_FILE(CLASSNAME)
 
 #  define FORTRILINOS_UNITTEST_MODULE_DEF(CLASSNAME) \
     FORTRILINOS_UNITTEST_MODULE_DEF0(CLASSNAME)
 
 #  define FORTRILINOS_UNITTEST_MODULE_BEGIN(CLASSNAME) \
+      implicit none BREAKLINE \
+      public BREAKLINE \
+      contains
+
+#  define FORTRILINOS_UNITTEST_SWITCH_BEGIN(CLASSNAME) \
     logical function select_test(which_test) result(success) BREAKLINE \
       character(len=50),intent(in) :: which_test BREAKLINE \
       success=.FALSE.
+
+#  define FORTRILINOS_UNITTEST_SWITCH_END(CLASSNAME) \
+    ; stop "Missing test. TEST FAILED" BREAKLINE \
+    endif BREAKLINE \
+    end function
 
 #  define FORTRILINOS_UNITTEST_MODULE_END0(CLASSNAME) \
     end module TEST_CALLS_FILE(CLASSNAME)
 
 #  define FORTRILINOS_UNITTEST_MODULE_END(CLASSNAME) \
-    ; stop "Missing test. TEST FAILED" BREAKLINE endif BREAKLINE end function BREAKLINE \
     FORTRILINOS_UNITTEST_MODULE_END0(CLASSNAME)
 
 /* Unittest level */
