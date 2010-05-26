00040ed364d1e9925470e9675eba95cbb0a5c630
Karla Morris <knmorri@sandia.gov>
2010-05-26 14:18:44 -0700

    RCP version of ForTrilinos using a defined assignment for each concrete class

--- src/epetra/FEpetra_Comm.F90
+++ src/epetra/FEpetra_Comm.F90
@@ -1,3 +1,40 @@
+!*********************************************************************
+! ForTrilinos: Object-Oriented Fortran 2003 interface to Trilinos
+!                Copyright 2010 Sandia Corporation
+!
+! Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+! the U.S. Government retains certain rights in this software.
+!
+! Redistribution and use in source and binary forms, with or without
+! modification, are permitted provided that the following conditions are met:
+!
+! 1. Redistributions of source code must retain the above copyright
+!    notice, this list of conditions and the following disclaimer.
+!
+! 2. Redistributions in binary form must reproduce the above copyright
+!    notice, this list of conditions and the following disclaimer in the
+!    documentation and/or other materials provided with the distribution.
+!
+! 3. Neither the name of the Corporation nor the names of the
+!    contributors may be used to endorse or promote products derived from
+!    this software without specific prior written permission.
+!
+! THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+! EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+! IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+! PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+! CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+! EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+! PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+! PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+! LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+! NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+! SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+!
+! Questions? Contact Karla Morris  (knmorri@sandia.gov) or 
+!                    Damian Rouson (rouson@sandia.gov)
+!*********************************************************************
+
 module FEpetra_Comm
   use ForTrilinos_universal_new ,only : universal
   use ForTrilinos_enums !,only: FT_Epetra_Comm_ID_t,ForTrilinos_Universal_ID_t
@@ -21,16 +58,8 @@ module FEpetra_Comm
     procedure                                     :: set_EpetraComm_ID
     procedure                 ,nopass             :: alias_EpetraComm_ID
     procedure ,non_overridable                    :: generalize_EpetraComm
-    procedure(EpetraComm_assign)        ,deferred :: Comm_assign 
-    generic  :: assignment(=)=>Comm_assign
-!    procedure                                     :: Comm_assign_ID
-!    procedure(EpetraSerialComm_assign)  ,deferred :: SerialComm_assign 
-!#ifdef HAVE_MPI
-!    procedure(EpetraMpiComm_assign)     ,deferred :: MpiComm_assign 
-!    generic :: assignment(=)=>MpiComm_assign,SerialComm_assign
-!#else
-!    generic :: assignment(=)=>SerialComm_assign
-!#endif /* HAVE_MPI */
+!    procedure(EpetraComm_assign)        ,deferred :: Comm_assign 
+!    generic  :: assignment(=)=>Comm_assign
     !Barrier Methods
     procedure(barrier_interface)          ,deferred          ::barrier
     !Broadcast Methods
@@ -70,23 +99,11 @@ module FEpetra_Comm
     !  class(Epetra_Comm) ,intent(in)  :: this
     !  class(Epetra_Comm) ,allocatable :: clone_interface
     !end function
-!    subroutine EpetraSerialComm_assign(lhs,rhs)
-!     import:: Epetra_Comm
-!      class(Epetra_Comm)             ,intent(inout) :: lhs
-!      class(Epetra_Comm)             ,intent(in) :: rhs
-!    end subroutine
-!#ifdef HAVE_MPI
-!    subroutine EpetraMpiComm_assign(lhs,rhs)
+!    subroutine EpetraComm_assign(lhs,rhs)
 !      import:: Epetra_Comm
-!      class(Epetra_Comm)             ,intent(inout) :: lhs
-!      class(Epetra_Comm)             ,intent(in) :: rhs
+!      class(Epetra_Comm) ,intent(inout) :: lhs
+!      class(Epetra_Comm) ,intent(in)    :: rhs
 !    end subroutine
-!#endif /* HAVE_MPI */
-    subroutine EpetraComm_assign(lhs,rhs)
-      import:: Epetra_Comm
-      class(Epetra_Comm) ,intent(inout) :: lhs
-      class(Epetra_Comm) ,intent(in)    :: rhs
-    end subroutine
     subroutine barrier_interface(this) 
       import:: Epetra_Comm
       class(Epetra_Comm) ,intent(in)  :: this
--- src/epetra/FEpetra_SerialComm.F90
+++ src/epetra/FEpetra_SerialComm.F90
@@ -1,3 +1,41 @@
+!*********************************************************************
+! ForTrilinos: Object-Oriented Fortran 2003 interface to Trilinos
+!                Copyright 2010 Sandia Corporation
+!
+! Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+! the U.S. Government retains certain rights in this software.
+!
+! Redistribution and use in source and binary forms, with or without
+! modification, are permitted provided that the following conditions are met:
+!
+! 1. Redistributions of source code must retain the above copyright
+!    notice, this list of conditions and the following disclaimer.
+!
+! 2. Redistributions in binary form must reproduce the above copyright
+!    notice, this list of conditions and the following disclaimer in the
+!    documentation and/or other materials provided with the distribution.
+!
+! 3. Neither the name of the Corporation nor the names of the
+!    contributors may be used to endorse or promote products derived from
+!    this software without specific prior written permission.
+!
+! THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+! EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+! IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+! PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+! CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+! EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+! PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+! PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+! LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+! NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+! SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+!
+! Questions? Contact Karla Morris  (knmorri@sandia.gov) or
+!                    Damian Rouson (rouson@sandia.gov)
+!*********************************************************************
+
+
 module FEpetra_SerialComm
   use ForTrilinos_enums ,only : FT_Epetra_Comm_ID,FT_Epetra_SerialComm_ID_t,ForTrilinos_Universal_ID_t
   use ForTrilinos_table_man
@@ -18,7 +56,7 @@ module FEpetra_SerialComm
      !procedure         :: clone 
      !Developers only
      procedure ,private:: remote_dealloc
-     procedure         :: Comm_assign
+     procedure         :: assign_hermetic=>Comm_assign
      procedure         :: get_EpetraSerialComm_ID 
      procedure ,nopass :: alias_EpetraSerialComm_ID
      procedure         :: generalize 
@@ -55,9 +93,8 @@ contains
    type(FT_Epetra_SerialComm_ID_t) ,intent(in) :: id
    type(FT_Epetra_Comm_ID_t)  :: id_t
    print *,'from_struct'
-   !from_struct%hermetic = hermetic()
+   from_struct%hermetic = hermetic()
    from_struct%SerialComm_id = id
-   call from_struct%hermetic%assign_hermetic(hermetic())
    call from_struct%set_EpetraComm_ID(from_struct%alias_EpetraComm_ID(from_struct%generalize()))
    print *,from_struct%SerialComm_id%table,from_struct%SerialComm_id%index,from_struct%SerialComm_id%is_const
    id_t=from_struct%get_EpetraComm_ID()
@@ -94,12 +131,12 @@ contains
 
   subroutine Comm_assign(lhs,rhs)
    class(Epetra_SerialComm),intent(inout) :: lhs
-   class(Epetra_Comm),intent(in)    :: rhs
+   class(hermetic),intent(in)    :: rhs
    print *,'Comm_assign in SerialComm'
    select type(rhs)
     class is (Epetra_SerialComm)
      lhs%SerialComm_id=rhs%SerialComm_id
-     call lhs%hermetic%assign_hermetic(rhs%hermetic)
+     lhs%hermetic=rhs%hermetic
      call lhs%set_EpetraComm_ID(rhs%get_EpetraComm_ID())    
    end select
   end subroutine
