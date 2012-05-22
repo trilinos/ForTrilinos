40eaf942022314852ac8e7619412c406640a858f
Karla Morris <knmorri@sandia.gov>
2012-05-22 09:33:42 -0700

    added allocatable attribute to variable within the main program. Change deals with mismatch in constructor definition

--- example/Concrete_Burgers_Central_Difference/main.F90
+++ example/Concrete_Burgers_Central_Difference/main.F90
@@ -360,7 +360,7 @@ program main
 #else
   type(Epetra_SerialComm) :: comm
 #endif
-  type(periodic_2nd_order) :: u,half_uu,u_half
+  type(periodic_2nd_order), allocatable :: u,half_uu,u_half
   procedure(initial_field) ,pointer :: initial 
   real(c_double) :: dt,half=0.5,t=0.,t_final=0.4,nu=1.
   real(c_double) :: t_start,t_end
