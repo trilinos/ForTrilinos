133ee40e471b1538ef79dadf4fadde33d4c5d0c4
Andrey Prokopenko <prokopenkoav@ornl.gov>
2017-02-24 19:15:34 -0500

    ForTrilinos: working on simplifying CMake intrastructure

--- src/simple/cmake/Dependencies.cmake
+++ src/simple/cmake/Dependencies.cmake
@@ -1,5 +1,12 @@
 TRIBITS_PACKAGE_DEFINE_DEPENDENCIES(
-  LIB_REQUIRED_PACKAGES Teuchos Tpetra Thyra ThyraTpetraAdapters Stratimikos ForTrilinosTeuchos
-  LIB_OPTIONAL_PACKAGES Ifpack2 MueLu
-  TEST_REQUIRED_TPLS MPI
+  LIB_REQUIRED_PACKAGES
+    Stratimikos
+    Teuchos
+    Thyra
+    ThyraTpetraAdapters
+    Tpetra
+    ForTrilinosTeuchos
+  LIB_OPTIONAL_PACKAGES
+    Ifpack2
+    MueLu
 )
