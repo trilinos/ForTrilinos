271ba5b5db45b919beff9c541160f5e93cf63de6
Andrey Prokopenko <prokopenkoav@ornl.gov>
2017-02-28 14:21:47 -0500

    ForTrilinos: adding code coverage

--- docker/jenkins/build.sh
+++ docker/jenkins/build.sh
@@ -7,7 +7,7 @@ cd ${TRILINOS_DIR}/packages/ForTrilinos
 [ -d build ] && rm -rf build
 mkdir build && cd build
 # configure trilinos with fortrilinos
-../scripts/docker_cmake
+../scripts/docker_cmake -D Trilinos_ENABLE_COVERAGE_TESTING=ON
 # build
 # build twice, as the first one fails with
 #       use TEST_CALLS_FILE
@@ -17,3 +17,9 @@ make -j${NPROC} -i
 make -j${NPROC} -i
 # run the unit tests
 ctest -j${NPROC} --no-compress-output -T Test
+# collect coverage data
+lcov --capture --directory packages/ForTrilinos --output-file lcov.info
+# upload it to codecov
+curl -s https://codecov.io/bash -o codecov_bash_uploader
+chmod +x codecov_bash_uploader
+./codecov_bash_uploader -Z -X gcov -f lcov.info
