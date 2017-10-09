0d9790df3f40409c1ce573ffa91542f514fb96af
Andrey Prokopenko <prokopenkoav@ornl.gov>
2017-10-09 15:35:46 -0400

    Framework: reorder subpackages now that SimpleInterface depends on Tpetra

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -2,6 +2,6 @@ TRIBITS_PACKAGE_DEFINE_DEPENDENCIES(
   SUBPACKAGES_DIRS_CLASSIFICATIONS_OPTREQS
   Epetra            src/epetra      SS OPTIONAL
   Teuchos           src/teuchos     SS OPTIONAL
-  SimpleInterface   src/simple      SS OPTIONAL
   Tpetra            src/tpetra      SS OPTIONAL
+  SimpleInterface   src/simple      SS OPTIONAL
 )
