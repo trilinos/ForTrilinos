9589cbed1f27ce80ab0a25dd68e28b21c4943f34
Seth R Johnson <johnsonsr@ornl.gov>
2020-09-01 16:19:58 -0400

    Update build script to enable testing

--- scripts/build-yuri.sh
+++ scripts/build-yuri.sh
@@ -16,6 +16,8 @@ cmake -G Ninja \
   -DCMAKE_INSTALL_PREFIX:PATH=$SOURCE_DIR/install \
   -DCMAKE_BUILD_TYPE:STRING=Debug \
   -DBUILD_SHARED_LIBS:BOOL=ON \
+  -DForTrilinos_EXAMPLES:BOOL=ON \
+  -DForTrilinos_TESTING:BOOL=ON \
   -DForTrilinos_USE_SWIG_Fortran:BOOL=ON \
   -DSWIG_EXECUTABLE:FILENAME=/rnsdhpc/code/swig-old/swig \
   -DSWIG_DIR:FILENAME=/rnsdhpc/code/swig-old/Lib \
